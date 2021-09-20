08b1f264f26579895672df8e572c8a9250e39255
narityan <86788282+narityan@users.noreply.github.com>
2021-09-20 19:42:27 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :item_name
+    validates :introduction
+    validates :category_id
+    validates :condition_id
+    validates :payer_id
+    validates :prefecture_id
+    validates :preparation_id
+    validates :price
+  end
 end
