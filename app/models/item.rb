03f2cf2f1772b0fd7d09d06bbc8a9a42e4b408fc
narityan <86788282+narityan@users.noreply.github.com>
2021-09-23 17:47:00 +0900

    プルダウン作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :payer
+  belongs_to :prefecture
+  belongs_to :preparation
 
   with_options presence: true do
     validates :item_name
     validates :introduction
-    validates :category_id
+    validates :category_id,   numericality: { other_than: 1 , message: "can't be blank"}
     validates :condition_id
     validates :payer_id
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :preparation_id
     validates :price
   end
