21300711e8403e98ab42dac07b8ab67101c03696
narityan <86788282+narityan@users.noreply.github.com>
2021-09-25 11:33:57 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,37 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.order("created_at DESC")
   end
   
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :introduction, :category_id, :condition_id, :payer_id, :prefecture_id, :preparation_id, :preice, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :introduction, :category_id, :condition_id, :payer_id, :prefecture_id, :preparation_id, :price, :image).merge(user_id: current_user.id)
   end
 end
