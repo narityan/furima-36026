02844db390935d124caa480c378f38326bdebdfa
narityan <86788282+narityan@users.noreply.github.com>
2021-10-09 11:19:56 +0900

    addressコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
   resources :purchases, only: [:index]
+  resources :addresses, only: [:index]
   end
 end
