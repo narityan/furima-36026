df9f271b1afcaa1de2a905d28cf6ef5ea0cda3a5
narityan <86788282+narityan@users.noreply.github.com>
2021-10-10 16:09:51 +0900

    購入画面への遷移

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-  resources :purchases, only: [:index]
+  resources :purchases, only: [:index, :create]
   resources :addresses, only: [:index]
   end
 end
