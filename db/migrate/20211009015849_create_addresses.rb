d589c32da945e26cbff4cee1d6e53a965c94a8fd
narityan <86788282+narityan@users.noreply.github.com>
2021-10-16 10:14:11 +0900

    カラムの訂正

--- db/migrate/20211009015849_create_addresses.rb
+++ db/migrate/20211009015849_create_addresses.rb
@@ -5,7 +5,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.integer       :prefecture_id,   null: false
       t.string        :city,            null: false
       t.string        :house_name,      null: false
-      t.string        :building_number
+      t.string        :building_name
       t.string        :phone_number,    null: false
       t.references    :purchase,        null: false, foreign_key: true
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_015849) do
+ActiveRecord::Schema.define(version: 2021_10_10_072314) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_10_09_015849) do
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "house_name", null: false
-    t.string "building_number"
+    t.string "building_name"
     t.string "phone_number", null: false
     t.bigint "purchase_id", null: false
     t.datetime "created_at", precision: 6, null: false
