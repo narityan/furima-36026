10823ab0d3ebd0b1a9d85f1ac48c35e0a9be99c2
narityan <86788282+narityan@users.noreply.github.com>
2021-11-02 20:46:19 +0900

    購入機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,6 +10,7 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path 
     else
@@ -20,7 +21,16 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :house_name, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
-
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :house_name, :building_name, :phone_number, ).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  
+      card: purchase_params[:token],    
+      currency: 'jpy'                 
+    )
+  end
+
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :house_name, :building_name, :phone_number, :item_id, :user_id
+  attr_accessor :post_code, :prefecture_id, :city, :house_name, :building_name, :phone_number, :item_id, :user_id, :token
 
   validates :prefecture_id, numericality: { other_than: 1 }
 
@@ -11,10 +11,11 @@ class PurchaseAddress
     validates :city
     validates :house_name
     validates :phone_number, format: {with: /\A[0-9]{11}\z/ }
+    validates :token
   end
 
   def save
-    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    purchase = Purchase.create(item_id: item_id, user_id: user_id,)
 
     Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_name: house_name, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
 
